944506da929284eda90bbb8e2cf9b9519a39a71f
Eemeli Aro <eemeli@gmail.com>
2018-04-02 14:11:52 +0300

    Add re-exporting index.js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as Message } from './message'
+export { default as MessageProvider } from './message-provider'
+export { default as withLocale } from './with-locale'
+export { default as withMessages } from './with-messages'
