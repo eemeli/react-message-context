c4021075e2b808986b80b4f3a161f3c49e7a86c9
Eemeli Aro <eemeli@gmail.com>
2019-07-15 10:10:13 +0300

    Oops, fix Babel config for tests

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,9 @@
+let modules = process.env.BABEL_TGT || false
+if (process.env.NODE_ENV === 'test') modules = 'auto'
+
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        loose: true,
-        modules: process.env.BABEL_TGT || false
-      }
-    ],
+    ['@babel/preset-env', { loose: true, modules }],
     '@babel/preset-react'
   ],
   plugins: ['@babel/plugin-proposal-class-properties']
