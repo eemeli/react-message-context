f3d209393d958c78c925e96b751f3225abdee74a
Eemeli Aro <eemeli@gmail.com>
2020-03-04 09:17:57 +0200

    Drop CommonJS build, exposing only the ES module endpoint

--- babel.config.js
+++ babel.config.js
@@ -6,20 +6,13 @@ const rmPropTypes = [
 ]
 
 switch (process.env.NODE_ENV) {
-  case 'cjs':
-    modules = 'auto'
-    plugins.push(rmPropTypes)
-    break
-  case 'esm':
-    modules = false
-    plugins.push(rmPropTypes)
-    break
   case 'ci':
   case 'test':
     modules = 'auto'
     break
   default:
-    throw new Error('NODE_ENV is required')
+    modules = false
+    plugins.push(rmPropTypes)
 }
 
 module.exports = {
