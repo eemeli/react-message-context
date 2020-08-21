e2a2efd9be877c729f6c9863355eacb749db1f10
Eemeli Aro <eemeli@gmail.com>
2020-08-21 14:19:10 +0300

    Simplify babel.config.js, as prod config is now in rollup.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,7 @@
-let modules
-const plugins = ['@babel/plugin-proposal-class-properties']
-const rmPropTypes = [
-  'transform-react-remove-prop-types',
-  { removeImport: true }
-]
-
-switch (process.env.NODE_ENV) {
-  case 'ci':
-  case 'test':
-    modules = 'auto'
-    break
-  default:
-    modules = false
-    plugins.push(rmPropTypes)
-}
-
 module.exports = {
   presets: [
-    ['@babel/preset-env', { loose: true, modules }],
+    ['@babel/preset-env', { loose: true, modules: 'auto' }],
     '@babel/preset-react'
   ],
-  plugins
+  plugins: ['@babel/plugin-proposal-class-properties']
 }
