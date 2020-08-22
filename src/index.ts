77dd2fd8556903ee0a03fc165d017c07deaebfe1
Eemeli Aro <eemeli@gmail.com>
2020-08-23 01:48:47 +0300

    Add api-extractor & api-documenter, generating docs & d.ts rollup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
+/**
+ * An efficient React front-end for message formatting libraries.
+ * Designed in particular for use with {@link https://messageformat.github.io | messageformat}, but will work with any messages.
+ * Provides the best possible API for a front-end developer, without making the back end any more difficult than it needs to be either.
+ * Should add at most about 1kB to your compiled & minified bundle size.
+ *
+ * @example
+ * ```js
+ * import {
+ *   getMessage,
+ *   getMessageGetter,
+ *   MessageContext,
+ *   MessageProvider,
+ *   Message,
+ *   useLocales,
+ *   useMessage,
+ *   useMessageGetter
+ * } from 'react-message-context'
+ * ```
+ *
+ * @packageDocumentation
+ */
 export { getMessage, getMessageGetter } from './get-message'
 export { Message, MessageProps } from './message'
 export { MessageContext } from './message-context'
